736d8caf4818554f2cc2d806d1b2581b398782a9
WMXPY <wm@mengw.io>
2019-04-15 00:02:23 -0500

    poke around

--- src/compress.ts
+++ src/compress.ts
@@ -4,9 +4,19 @@
  * @description Compress
  */
 
+import * as Fs from 'fs';
 import { createGzip, Gzip } from 'zlib';
 
-export const compress = (source: string, target: string) => {
+export const compress = () => {
 
     const gzip: Gzip = createGzip();
+
+    // const originFile: Fs.ReadStream = Fs.createReadStream('./test.a');
+    const targetFile: Fs.WriteStream = Fs.createWriteStream('./test.gz');
+
+    // originFile.pipe(gzip);
+    gzip.pipe(targetFile);
+    gzip.write('hello ds da sad asd asd');
 };
+
+compress();
